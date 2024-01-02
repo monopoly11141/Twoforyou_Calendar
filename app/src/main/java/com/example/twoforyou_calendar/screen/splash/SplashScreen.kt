6759d563bd564895013f5d13d9af09fb4aa88bb2
monopoly11141 <monopoly1114@vt.edu>
2024-01-02 10:10:34 +0900

    Update SplashScreen.kt
    basic splash screen done

--- app/src/main/java/com/example/twoforyou_calendar/screen/splash/SplashScreen.kt
+++ app/src/main/java/com/example/twoforyou_calendar/screen/splash/SplashScreen.kt
@@ -1,8 +1,22 @@
 package com.example.twoforyou_calendar.screen.splash
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.twoforyou_calendar.navigation.Screen
 import kotlinx.coroutines.delay
@@ -11,11 +25,40 @@ import kotlinx.coroutines.delay
 fun SplashScreen(
     navController: NavController
 ) {
-    Text("Splash Screen")
 
+    var splashText by remember { mutableStateOf("") }
+    var splashFinishText = "쉬운 일정앱"
+
+    Box(
+        modifier = Modifier
+            .padding(10.dp)
+            .background(
+                Color.Gray,
+                shape = CircleShape
+            )
+            .aspectRatio(1f),
+        contentAlignment = Alignment.Center
+    ) {
+
+        Text(
+            text = splashText,
+            textAlign = TextAlign.Center,
+            color = Color.White
+        )
+
+    }
 
     LaunchedEffect(key1 = true) {
         delay(500L)
         navController.navigate(route = Screen.HomeScreen.route)
     }
+
+    LaunchedEffect(true) {
+        while (splashFinishText.isNotEmpty()) {
+            splashText += splashFinishText.first()
+            splashFinishText = splashFinishText.drop(1)
+            delay(100L)
+        }
+    }
+
 }
